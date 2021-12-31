680673c6063a63ba2fa1a2dcdcd419a2a0478dea
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-31 02:47:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("  A software object can store data and perform operations. The data stored in an object are commonly called fields and the operations that an object can perform are called methods."); 
+    System.out.println(" ");
+    System.out.println("  A class contains fields and methods that define the behavior of the object. You can think of a class as a blueprint that can be used to create a particular type of object.");
+System.out.println(" ");
+    System.out.println("  programmers use a programming language (such as Java) to describe the program as various objects. When the program is run, the object are created, and they start doing things by running their methods.");
+  }
+}
